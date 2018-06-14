210d05c839a7fc97e4582cf82afff36eed015a9b
Nikhil Wankhede <nikhil@kredx.com>
2018-06-14 16:56:30 +0530

    Updated UX

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ $(function(){
   
   var payment_term_control = widget.find(".payment_term_control").first();
 
+  var invoice_amount_range_control = widget.find(".invoice_amount_range_control").first();
   var invoice_amount_control = widget.find(".invoice_amount_control").first();
-  var invoice_amount_unit_control = widget.find(".invoice_amount_unit_control").first();
+  var invoice_amount_range = widget.find("[name='invoice_amount_range']").first();
+  var invoice_amount_in_crore_checkbox = widget.find("[name='invoice_amount_above_a_crore']").first();
   var fees_amount_control = widget.find(".fees_amount_control").first();
   var cash_amount_control = widget.find(".cash_amount_control").first();
 
@@ -15,19 +17,20 @@ $(function(){
   var invoice_amount_unit = widget.find("[name='invoice_amount_unit']");
   var fees_amount = widget.find("[name='fees_amount']").first();
   var cash_amount = widget.find("[name='cash_amount']").first();
+  var invoice_amount_label = widget.find("[name='invoice_amount_label']").first();
+  
   
   var payment_term_error = payment_term_control.find(".error").first();
   var invoice_amount_error = invoice_amount_control.find(".error").first();
-  var invoice_amount_unit_error = invoice_amount_unit_control.find(".error").first();
 
-  
   var render = debounce(update, 100);
   
   payment_term.on('change',render);
   invoice_amount.on('change',render);
   invoice_amount.on('keydown',render);
   invoice_amount_unit.on('change',render);
-
+  invoice_amount_range.on('input change', render);
+  invoice_amount_in_crore_checkbox.on('change',render);
 
   widget.find('.amount').on('keyup keypress blur change', function(event){
     var value = event.target.value;
@@ -43,8 +46,25 @@ $(function(){
     
     payment_term_value = payment_term.filter(":checked").first();
     invoice_amount_unit_value = invoice_amount_unit.filter(":checked").first();
-    invoice_amount_value = invoice_amount;
+
+    is_invoice_amount_in_crore = invoice_amount_in_crore_checkbox.prop('checked');  
     
+  
+    if(is_invoice_amount_in_crore){
+      invoice_amount_value = Number(currencyParser(invoice_amount.val()));
+      invoice_amount_range_control.hide();
+      invoice_amount_control.show();
+    }
+    else{
+      invoice_amount_control.hide();
+      invoice_amount_range_control.show();
+      invoice_amount_value = Number(currencyParser(invoice_amount_range.val())) * 100000;
+      invoice_amount.val(formatINR(invoice_amount_value));
+    }
+
+    invoice_amount_label.html(formatINR(invoice_amount_value/100000) + " Lakhs");
+
+
     if(!payment_term_value.val()){
       payment_term_error.addClass("has-error");
       payment_term_error.html("Please select payment term.");
@@ -54,14 +74,14 @@ $(function(){
     }
     
     var invoice_amount_valid = true;
-    var invoice_amount_val = currencyParser(invoice_amount_value.val());
+    var invoice_amount_val = invoice_amount_value;
     
     if(!invoice_amount_val){
       invoice_amount_valid = false;
       invoice_amount_error.addClass("has-error");
       invoice_amount_error.html("Please enter amount.");
     }else{
-      if( invoice_amount_val>10000 ){
+      if( invoice_amount_val>1000000000000 ){
         invoice_amount_valid = false;
         invoice_amount_error.addClass("has-error");
         invoice_amount_error.html("Amount should be less than 10000.");
@@ -74,32 +94,15 @@ $(function(){
         invoice_amount_error.html("");
       }
     }
-    
-    if(!invoice_amount_unit_value.val()){
-      invoice_amount_unit_error.addClass("has-error");
-      invoice_amount_unit_error.html("Please select amount in.");
-    }else{
-      invoice_amount_unit_error.removeClass("has-error");
-      invoice_amount_unit_error.html("");
-    }
-    
-    
-    if(invoice_amount_valid && payment_term_value.val() && invoice_amount_val && invoice_amount_unit_value.val()){
+        
+    if(invoice_amount_valid && payment_term_value.val() && invoice_amount_val){
         var payment_term_val = parseInt(payment_term_value.val());
-        var invoice_amount_unit_val = invoice_amount_unit_value.val();
-        
-        if(invoice_amount_unit_val === "lacs"){
-          invoice_amount_val = invoice_amount_val * 100000;
-        }
-        else if(invoice_amount_unit_val === "crores"){
-          invoice_amount_val = invoice_amount_val * 10000000;
-        }
-        
+
         var fees_amount_val = invoice_amount_val * 0.001;
         var cash_amount_val = invoice_amount_val * 0.8;
         
-        fees_amount.html(formatINR(fees_amount_val.toFixed(2)));
-        cash_amount.html(formatINR(cash_amount_val.toFixed(2)));
+        fees_amount.html(formatINR(fees_amount_val.toFixed(0)));
+        cash_amount.html(formatINR(cash_amount_val.toFixed(0)));
       
     }else{
       fees_amount.html('₹ 0');
